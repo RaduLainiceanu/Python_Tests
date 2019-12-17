92fdeb6d4dbd3277bfe5fa2ceb0ff514116bccd7
RaduLainiceanu <48065765+RaduLainiceanu@users.noreply.github.com>
2019-12-17 19:24:38 +0200

    Create string_format_2si3.py

---
+++ string_format_2si3.py
@@ -0,0 +1,16 @@
+'''Autor: Lainiceanu Radu-Constantin
+Data: 17.12.2019
+            Formatarea String-urilor in Python 2 si 3
+Infografie: Curs Complet Python 2 si 3, Mihai Catalin Teodosiu @ Udemy
+'''
+#Scris pentru interpretor Python3
+'''
+#In Python2 asta era sintaxa corecta:
+print "Python %d este varianta moderna a Python %s" % (3, 2)
+#sau
+print "Python {} este varianta moderna a Python {}".format(3, 2)
+'''
+#In Python3 sintaxa corecta este
+print ("Python %d este varianta moderna a Python %s" % (3, 2) )
+#si
+print ("Python {} este varianta moderna a Python {}".format(3, 2) )
